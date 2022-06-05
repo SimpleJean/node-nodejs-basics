b3716082dc7f6f7608d6468366555504725a4337
Jean <zhan.yessim@gmail.com>
2022-06-05 23:08:20 +0600

    feat: hash task is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = path.join('./files/fileToCalculateHashFor.txt');
+  const hash = crypto.createHash('sha256');
+  const stream = fs.createReadStream(pathToFile);
+
+  stream.on('readable', (data) => {
+    const chunk = stream.read();
+    if (chunk) {
+      hash.update(chunk);
+    } else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
+calculateHash();
+
+//TODO: Maybe need to use async await and convert to HEX
