959832a0d2e239220d2804f89013dfc42639933a
Jean <zhan.yessim@gmail.com>
2022-06-05 23:09:11 +0600

    feat: module task is done

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,16 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import path from 'path';
+import module from 'module';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
+const jsonC = await readFile('./files/c.js', 'utf-8');
 
 let unknownObject;
+const jsonA = JSON.parse(await readFile('./files/a.json', 'utf-8'));
+
+const jsonB = JSON.parse(await readFile('./files/b.json', 'utf-8'));
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = jsonA;
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = jsonB;
 }
 
 console.log(`Release ${release()}`);
@@ -21,11 +31,10 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const createMyServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+  res.end('Request accepted');
 });
 
 module.exports = {
-    unknownObject,
-    createMyServer,
+  unknownObject,
+  createMyServer,
 };
-
