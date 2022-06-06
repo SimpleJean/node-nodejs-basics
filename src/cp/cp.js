69177f3b53ab4a25c1d2ebf8c5d7c8c112808888
Jean <zhan.yessim@gmail.com>
2022-06-06 15:44:25 +0600

    feat: cp task is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import cp from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToScript = path.join('./files/script.js');
+
+  const child = cp.fork(pathToScript);
+  child.on('message', (chunk) => {
+    console.log(`Received from child process: ${chunk.toString()}`);
+  });
+  child.send('data');
+
+  child.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+
+  //console.log(`${child}`);
 };
+spawnChildProcess();
